5d8d3e40fd0758d5a42f842421a16cfe8a5ece9c
Michael Kang <43582949+michaelkang84@users.noreply.github.com>
2026-02-26 14:18:23 -0500

    IGW + route table and association

--- terraform/06-resources/README.md
+++ terraform/06-resources/README.md
@@ -1,5 +1,5 @@
 Steps
-1. deployd a VPC and a Subnet
+1. deployd a VPC and a Subnet [done]
 2. Deploy an IGW and associate it with the VPC
 3. Setup route table with a route to the IGW
 4. Deploy EC2 isntance inside the created Subnet
--- terraform/06-resources/networking.tf
+++ terraform/06-resources/networking.tf
@@ -16,3 +16,32 @@ resource "aws_subnet" "public" {
     Project   = "06-resources"
   }
 }
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name      = "06-resources-main-igw"
+    ManagedBy = "Terraform"
+    Project   = "06-resources"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name      = "06-resources-public-rtb"
+    ManagedBy = "Terraform"
+    Project   = "06-resources"
+  }
+}  
+
+resource "aws_route_table_association" "public" {
+  subnet_id = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}

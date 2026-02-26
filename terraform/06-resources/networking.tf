419e59f9d707d09b160fbdf2602e74640fc44d3b
Michael Kang <43582949+michaelkang84@users.noreply.github.com>
2026-02-26 14:22:57 -0500

    locals common tags

--- terraform/06-resources/networking.tf
+++ terraform/06-resources/networking.tf
@@ -1,29 +1,30 @@
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name      = "06-resources"
+locals {
+  common_tags = {
     ManagedBy = "Terraform"
     Project   = "06-resources"
   }
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = merge(local.common_tags, {
+    Name = "06-resources"
+  })
 }
 
 resource "aws_subnet" "public" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
-  tags = {
-    Name      = "06-resources-public"
-    ManagedBy = "Terraform"
-    Project   = "06-resources"
-  }
+  tags = merge(local.common_tags, {
+    Name = "06-resources-public"
+  })
 }
 
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
-  tags = {
-    Name      = "06-resources-main-igw"
-    ManagedBy = "Terraform"
-    Project   = "06-resources"
-  }
+  tags = merge(local.common_tags, {
+    Name = "06-resources-main-igw"
+  })
 }
 
 resource "aws_route_table" "public" {
@@ -34,14 +35,12 @@ resource "aws_route_table" "public" {
     gateway_id = aws_internet_gateway.main.id
   }
 
-  tags = {
-    Name      = "06-resources-public-rtb"
-    ManagedBy = "Terraform"
-    Project   = "06-resources"
-  }
-}  
+  tags = merge(local.common_tags, {
+    Name = "06-resources-public-rtb"
+  })
+}
 
 resource "aws_route_table_association" "public" {
-  subnet_id = aws_subnet.public.id
+  subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
 }

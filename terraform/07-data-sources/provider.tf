1a9bbaff0fdb9d7dbd13c7bfa7180f675d9e8427
Michael Kang <43582949+michaelkang84@users.noreply.github.com>
2026-02-27 17:12:33 -0500

    aws ssm parameter data source reference

--- terraform/07-data-sources/provider.tf
+++ terraform/07-data-sources/provider.tf
@@ -12,7 +12,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-provider "aws" {
-  region = "us-west-1"
-  alias  = "us-west"
-}
+# provider "aws" {
+#   region = "us-west-1"
+#   alias  = "us-west"
+# }

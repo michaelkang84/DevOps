91bc385834fb4a0849cefed6479a687cae2ada0e
Michael Kang <43582949+michaelkang84@users.noreply.github.com>
2026-03-01 17:50:51 -0500

    local cross file usage

--- terraform/08-input-vars-locals-outputs/compute.tf
+++ terraform/08-input-vars-locals-outputs/compute.tf
@@ -1,12 +1,13 @@
 locals {
+  project       = "08-input-vars-locals-outputs"
   project_owner = "terraform-course"
   cost_center   = "1234"
   managed_by    = "Terraform"
-
 }
 
 locals {
   common_tags = {
+    project     = local.project
     owner       = local.project_owner
     cost_center = local.cost_center
     managed_by  = local.managed_by
---
+++ terraform/08-input-vars-locals-outputs/s3.tf
@@ -0,0 +1,12 @@
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
+}
+
+resource "aws_s3_bucket" "project_bucket" {
+  bucket = "${local.project}-bucket-${random_id.bucket_suffix.hex}"
+
+  tags = merge(
+    local.common_tags,
+    var.additional_tags
+  )
+}  

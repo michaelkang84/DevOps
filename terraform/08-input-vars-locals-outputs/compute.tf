ccafb4afae0011cc8e56a643dcd15ec371442317
Michael Kang <43582949+michaelkang84@users.noreply.github.com>
2026-03-01 17:52:07 -0500

    shared locals file

--- terraform/08-input-vars-locals-outputs/compute.tf
+++ terraform/08-input-vars-locals-outputs/compute.tf
@@ -1,19 +1,3 @@
-locals {
-  project       = "08-input-vars-locals-outputs"
-  project_owner = "terraform-course"
-  cost_center   = "1234"
-  managed_by    = "Terraform"
-}
-
-locals {
-  common_tags = {
-    project     = local.project
-    owner       = local.project_owner
-    cost_center = local.cost_center
-    managed_by  = local.managed_by
-  }
-}
-
 data "aws_ami" "ubuntu-east" {
   most_recent = true
   owners      = ["099720109477"] # Cononical

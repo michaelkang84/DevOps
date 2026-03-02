04c46ed0068547ba121163b76607ad82ac54739a
Michael Kang <43582949+michaelkang84@users.noreply.github.com>
2026-03-02 12:29:41 -0500

    splat for map with values function

--- terraform/09-expressions/splat.tf
+++ terraform/09-expressions/splat.tf
@@ -1,7 +1,13 @@
 locals {
   firstnames_from_splat = var.players[*].name
+
+  sponsors_from_splat = values(local.players_map_two)[*].sponsors
 }
 
 output "firstnames_from_splat" {
   value = local.firstnames_from_splat
+}
+
+output "sponsors_from_splat" {
+  value = local.sponsors_from_splat
 }

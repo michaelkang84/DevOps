04c0f99297620c1426a298559ac85e3329681a85
Michael Kang <43582949+michaelkang84@users.noreply.github.com>
2026-03-02 13:22:47 -0500

    more function examples

--- terraform/10-functions/locals.tf
+++ terraform/10-functions/locals.tf
@@ -1,14 +1,28 @@
 locals {
   name = "Michael Kang"
   age  = 31
+
+  my_object = {
+    key1 = 10
+    key2 = "my_value"
+  }
 }
 
 output "example_one" {
   value = startswith(lower(local.name), "mi")
-#   value = upper(local.name)
+  #   value = upper(local.name)
 }
 
 output "example_two" {
-    value = pow(local.age, 2)
-    # value = abs(-local.age)
+  value = pow(local.age, 2)
+  # value = abs(-local.age)
+}
+
+output "example_three" {
+    value = yamldecode(file("${path.module}/users.yaml")).users[*].name
+}
+
+output "example_four" {
+    value = jsonencode(local.my_object)
+    # value = yamlencode(local.my_object)
 }

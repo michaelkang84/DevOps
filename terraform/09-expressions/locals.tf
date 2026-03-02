dd7b360c537c1a5c3548f4dd79b47fe3da55b393
Michael Kang <43582949+michaelkang84@users.noreply.github.com>
2026-03-02 11:21:31 -0500

    expression types

---
+++ terraform/09-expressions/locals.tf
@@ -0,0 +1,6 @@
+locals {
+    math = 2 * 2 # Math operators: *, /, +, -
+    equality = 2 == 2 # Equality operators: ==, !=
+    comparison = 2 > 1 # Comparison operators: >, <, >=, <=
+    logical = true && false # Logical operators: &&, ||, !
+}

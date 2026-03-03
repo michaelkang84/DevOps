ea24df13c64aedcbeeba2322072c32129c15819d
Michael Kang <43582949+michaelkang84@users.noreply.github.com>
2026-03-03 09:19:58 -0500

    instance ami validation

--- terraform/11-multiple-resources/variables.tf
+++ terraform/11-multiple-resources/variables.tf
@@ -19,9 +19,14 @@ variable "ec2_instance_config_list" {
   # 2. Map from the instance_type to a boolean indicating whether it's valid or not
   # 3. Check whether list of booleans contain only true values
 
+  # Ensure that only t2.micro instances are allowed
   validation {
     condition     = alltrue([for config in var.ec2_instance_config_list : contains(["t2.micro"], config.instance_type)])
     error_message = "Only t2.micro instances are allowed."
+  }
 
+  validation {
+    condition     = alltrue([for config in var.ec2_instance_config_list : contains(["ubuntu", "nginx"], config.ami)])
+    error_message = "Only 'ubuntu' and 'nginx' AMIs are allowed."
   }
 }

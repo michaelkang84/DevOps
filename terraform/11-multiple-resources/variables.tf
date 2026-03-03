5c926be51e20964828007cebbba99327a831a3c4
Michael Kang <43582949+michaelkang84@users.noreply.github.com>
2026-03-03 09:51:33 -0500

    instances from map validations

--- terraform/11-multiple-resources/variables.tf
+++ terraform/11-multiple-resources/variables.tf
@@ -14,6 +14,22 @@ variable "ec2_instance_config_map" {
     ami           = string
     subnet_index  = optional(number, 0) # Optional subnet index with a default value of 0
   }))
+
+  validation {
+    condition     = alltrue([for config in values(var.ec2_instance_config_map) : contains(["t2.micro"], config.instance_type)])
+    error_message = "Only t2.micro instances are allowed."
+  }
+
+  validation {
+    condition     = alltrue([for config in values(var.ec2_instance_config_map) : contains(["ubuntu", "nginx"], config.ami)])
+    error_message = "Only 'ubuntu' and 'nginx' AMIs are allowed."
+  }
+
+  # Could be invalid since terraform might not know how many subnets are going to be created at time of validation
+  #   validation {
+  #     condition     = alltrue([for config in values(var.ec2_instance_config_map) : config.subnet_index <= length(aws_subnet.main)])
+  #     error_message = "Inavlid Subnet index"
+  #   }
 }
 
 variable "ec2_instance_config_list" {

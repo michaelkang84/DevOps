1541183268626c46a52e6374f77b6ab18ba1726b
Michael Kang <43582949+michaelkang84@users.noreply.github.com>
2026-03-03 09:08:30 -0500

    instances from list

--- terraform/11-multiple-resources/compute.tf
+++ terraform/11-multiple-resources/compute.tf
@@ -1,3 +1,10 @@
+locals {
+  ami_ids = {
+    ubuntu = data.aws_ami.ubuntu-east.id,
+    nginx  = data.aws_ami.nginx_image.id
+  }
+}
+
 data "aws_ami" "ubuntu-east" {
   most_recent = true
   owners      = ["099720109477"] # Cononical
@@ -13,18 +20,45 @@ data "aws_ami" "ubuntu-east" {
   }
 }
 
-resource "aws_instance" "from_count" {
-  count         = var.ec2_instance_count
-  ami           = data.aws_ami.ubuntu-east.id
-  instance_type = "t2.micro"
+data "aws_ami" "nginx_image" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["bitnami-nginx-1.28.2-*-debian-12-amd64-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+}
+
+resource "aws_instance" "from_list" {
+  count         = length(var.ec2_instance_config_list)
+  ami           = local.ami_ids[var.ec2_instance_config_list[count.index].ami]
+  instance_type = var.ec2_instance_config_list[count.index].instance_type
   subnet_id     = aws_subnet.main[count.index % length(aws_subnet.main)].id
-  # 0 % 4 = 0
-  # 1 % 4 = 1
-  # 2 % 4 = 2
-  # 3 % 4 = 1
 
   tags = {
     Name    = "${local.project}-instance-${count.index}"
     Project = local.project
   }
 }
+
+# resource "aws_instance" "from_count" {
+#   count         = var.ec2_instance_count
+#   ami           = data.aws_ami.ubuntu-east.id
+#   instance_type = "t2.micro"
+#   subnet_id     = aws_subnet.main[count.index % length(aws_subnet.main)].id
+#   # 0 % 4 = 0
+#   # 1 % 4 = 1
+#   # 2 % 4 = 2
+#   # 3 % 4 = 1
+
+#   tags = {
+#     Name    = "${local.project}-instance-${count.index}"
+#     Project = local.project
+#   }
+# }
--- terraform/11-multiple-resources/terraform.tfvars
+++ terraform/11-multiple-resources/terraform.tfvars
@@ -1,3 +1,14 @@
-subnet_count = 3
+# subnet_count = 2
 
-ec2_instance_count = 4
+ec2_instance_count = 0
+
+ec2_instance_config_list = [
+  {
+    instance_type = "t2.micro"
+    ami           = "ubuntu"
+  },
+  {
+    instance_type = "t2.micro"
+    ami           = "nginx"
+  }
+]

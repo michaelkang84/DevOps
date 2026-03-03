0676e7ce1ca3fab0dcd98e8200a09a5f44b46ee0
Michael Kang <43582949+michaelkang84@users.noreply.github.com>
2026-03-03 12:38:11 -0500

    iam user login profile resources

---
+++ terraform/proj02-iam-users/users.tf
@@ -0,0 +1,29 @@
+locals {
+  users_from_yaml = yamldecode(file("${path.module}/user-roles.yaml")).users
+}
+
+resource "aws_iam_user" "users" {
+  for_each = toset(local.users_from_yaml[*].username)
+  name     = each.value
+}
+
+resource "aws_iam_user_login_profile" "user_login_profiles" {
+  for_each        = aws_iam_user.users
+  user            = each.value.name
+  password_length = 8
+
+  lifecycle {
+    ignore_changes = [password_length, password_reset_required, pgp_key]
+  }
+}
+
+output "passwords" {
+    sensitive = true
+    value     = {
+        for user in aws_iam_user.users : user.name => aws_iam_user_login_profile.user_login_profiles[user.name].password
+    }
+}
+
+output "users" {
+  value = local.users_from_yaml
+}

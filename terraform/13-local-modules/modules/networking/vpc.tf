4245e189aa97de4379fa099914afbc42177512e2
Michael Kang <43582949+michaelkang84@users.noreply.github.com>
2026-03-04 11:16:40 -0500

    public RTB and RTB associate

--- terraform/13-local-modules/modules/networking/vpc.tf
+++ terraform/13-local-modules/modules/networking/vpc.tf
@@ -43,3 +43,21 @@ resource "aws_internet_gateway" "this" {
   count = length(keys(local.public_subnets)) > 0 ? 1 : 0
 
 }
+
+resource "aws_route_table" "public_rtb" {
+  count = length(keys(local.public_subnets)) > 0 ? 1 : 0
+
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this[0].id
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  for_each = local.public_subnets
+
+  subnet_id = aws_subnet.this[each.key].id
+  route_table_id = aws_route_table.public_rtb[0].id
+}
